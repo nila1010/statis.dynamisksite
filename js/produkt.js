99d9bf30ded85d79a588a852be2d60fb42c36675
NL <nila0001@stud.kea.dk>
2023-08-24 15:09:33 +0200

    v2

--- js/produkt.js
+++ js/produkt.js
@@ -1,4 +1,7 @@
-fetch("https://kea-alt-del.dk/t7/api/products/1163")
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+fetch("https://kea-alt-del.dk/t7/api/products/" + id)
   .then((response) => response.json())
   .then((data) => visProdukt(data));
 
